1ddf220164dc905222fd07c7b8c1791e9eda38ce
PrajaktaJoshi06 <150420787+PrajaktaJoshi06@users.noreply.github.com>
2024-08-14 17:00:24 -0400

    Time: 2542 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -3,4 +3,4 @@ SELECT r.contest_id, round(count(distinct r.user_id)/count(distinct u.user_id) *
 from Users u
 JOIN Register r 
 group by r.contest_id
-order by percentage desc, contest_id
+order by percentage desc, contest_id;
