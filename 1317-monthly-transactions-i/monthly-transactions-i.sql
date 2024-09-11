5d5afd90b5d5fcf2b13fe42fb73505ec0a434e73
PrajaktaJoshi06 <150420787+PrajaktaJoshi06@users.noreply.github.com>
2024-09-11 01:38:59 -0400

    Time: 566 ms (68.80%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -3,6 +3,6 @@ SELECT  DATE_FORMAT(trans_date, "%Y-%m")as month, country,
 COUNT(id) as trans_count, coalesce(SUM(CASE WHEN state = 'approved' then 1 END),0) as approved_count,
 SUM(amount) as trans_total_amount, coalesce(SUM(CASE WHEN state = 'approved' then amount END),0) as approved_total_amount
 FROM Transactions
-Group BY 1,2
+Group BY month,country
 
 #concat(extract(year from trans_date), '-', extract(month from trans_date))
