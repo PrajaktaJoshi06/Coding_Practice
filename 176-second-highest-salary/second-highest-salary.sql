a0192aadc389d7b5a0d9cc0e98cd0825cc43769e
PrajaktaJoshi06 <150420787+PrajaktaJoshi06@users.noreply.github.com>
2024-09-14 18:15:03 -0400

    Time: 223 ms (85.86%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -4,24 +4,24 @@
 -- Join Employee e2
 -- on e1.salary < e2.salary
 
--- SELECT MAX(salary) as SecondHighestSalary
--- FROM Employee 
--- where salary not in 
--- (SELECT MAX(salary) as SecondHighestSalary
--- FROM Employee)
+SELECT MAX(salary) as SecondHighestSalary
+FROM Employee 
+where salary not in 
+(SELECT MAX(salary) as SecondHighestSalary
+FROM Employee)
 
-WITH ranked_sal AS (
-    SELECT 
-        id, 
-        salary AS SecondHighestSalary,
-        DENSE_RANK() OVER (ORDER BY salary DESC) AS rank_sal
-    FROM Employee
-)
+-- WITH ranked_sal AS (
+--     SELECT 
+--         id, 
+--         salary AS SecondHighestSalary,
+--         DENSE_RANK() OVER (ORDER BY salary DESC) AS rank_sal
+--     FROM Employee
+-- )
 
-SELECT 
-    MAX(CASE 
-        WHEN rank_sal = 2 THEN SecondHighestSalary 
-        ELSE NULL 
-    END) AS SecondHighestSalary
-FROM ranked_sal;
+-- SELECT 
+--     MAX(CASE 
+--         WHEN rank_sal = 2 THEN SecondHighestSalary 
+--         ELSE NULL 
+--     END) AS SecondHighestSalary
+-- FROM ranked_sal;
 
