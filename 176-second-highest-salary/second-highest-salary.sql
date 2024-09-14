ae4010036e2be4369701022bb11ad6d742853187
PrajaktaJoshi06 <150420787+PrajaktaJoshi06@users.noreply.github.com>
2024-09-14 18:14:22 -0400

    Time: 237 ms (70.04%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,27 @@
-# Write your MySQL query statement below
-SELECT MAX(salary) as SecondHighestSalary
-FROM Employee 
-where salary not in 
-(SELECT MAX(salary) as SecondHighestSalary
-FROM Employee)
+-- # Write your MySQL query statement below
+-- SELECT MAX(e1.salary) as SecondHighestSalary
+-- FROM Employee e1 
+-- Join Employee e2
+-- on e1.salary < e2.salary
+
+-- SELECT MAX(salary) as SecondHighestSalary
+-- FROM Employee 
+-- where salary not in 
+-- (SELECT MAX(salary) as SecondHighestSalary
+-- FROM Employee)
+
+WITH ranked_sal AS (
+    SELECT 
+        id, 
+        salary AS SecondHighestSalary,
+        DENSE_RANK() OVER (ORDER BY salary DESC) AS rank_sal
+    FROM Employee
+)
+
+SELECT 
+    MAX(CASE 
+        WHEN rank_sal = 2 THEN SecondHighestSalary 
+        ELSE NULL 
+    END) AS SecondHighestSalary
+FROM ranked_sal;
+
